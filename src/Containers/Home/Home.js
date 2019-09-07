f16d11cd5fe5d3ffdefcd6e63e4c706fd307ddf6
Harika Tirunagari <36506029+Tirunagari-Harika@users.noreply.github.com>
2019-09-08 02:38:40 +0800

    Update Home.js

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import * as questionActions from "../../store/actionCreators/questionActions";
-import * as userActions from "../../store/actionCreators/userActions";
+/* import * as userActions from "../../store/actionCreators/userActions"; */
 import { connect } from "react-redux";
 
 class Home extends Component{
     componentDidMount(){
-
+        this.props.getQuestions(this.props.currentUser.id);
     }
     
     render(){
-
+        console.log(this.props);
         return(<div>Home</div>)
     }
 }
@@ -17,13 +17,14 @@ class Home extends Component{
 const mapStateToProps = (state) => {
     console.log("Home ",state);
     return {
-       
+       currentUser: state.currentUser.currentUser,
+       questions: state.questions.questions
     }
 }
 
-const mapDispatchToProps = () => {
+const mapDispatchToProps = (dispatch) => {
     return {
-
+        getQuestions: (currentUserId) => dispatch(questionActions.getquestionsActions(currentUserId))
     }
 }
 
